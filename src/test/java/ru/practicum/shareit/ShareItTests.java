bf37370cedb2526545652fd18943ece3f36c1de0
Lev <leviksun.s@gmail.com>
2026-05-15 11:24:39 +0300

    попытка

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,9 +1,7 @@
 package ru.practicum.shareit;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
