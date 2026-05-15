77e0dea3f358825774ee56511fb9a8040932ffda
Lev <leviksun.s@gmail.com>
2026-05-15 17:01:08 +0300

    внёс правки в чекстайл

--- src/main/java/ru/practicum/shareit/Exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/Exception/ErrorHandler.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.Exception;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.LoggerFactoryFriend;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,6 +14,7 @@ import java.util.Map;
 @RestControllerAdvice
 public class ErrorHandler {
     private static final Logger logger = LoggerFactory.getLogger(ErrorHandler.class);
+
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleIllegalArgument(final IllegalArgumentException e) {
